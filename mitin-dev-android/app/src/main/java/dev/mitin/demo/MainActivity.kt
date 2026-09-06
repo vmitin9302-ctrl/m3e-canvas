1c4a5317756ec500d17580f170a30da911176147
vmitin9302-ctrl <vmitin9302@gmail.com>
2026-09-06 18:53:40 +0300

    fix(android): use compatible dark system bar handling on Android 8

--- mitin-dev-android/app/src/main/java/dev/mitin/demo/MainActivity.kt
+++ mitin-dev-android/app/src/main/java/dev/mitin/demo/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.mitin.demo
 
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
@@ -30,7 +31,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
+        )
         setContent { MitinTheme { MitinApp() } }
     }
 }
